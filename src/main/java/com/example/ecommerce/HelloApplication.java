c2e6126f2a04dadfb5baefc4a6f8f0c6d2b8c8ab
0001153030 <0001153030@senaimgaluno.com.br>
2026-02-03 11:08:19 -0300

    menu principal

--- src/main/java/com/example/ecommerce/HelloApplication.java
+++ src/main/java/com/example/ecommerce/HelloApplication.java
@@ -10,9 +10,11 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                HelloApplication.class.getResource("/fxml/menu-principal.fxml")
+        );
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("Sistema com Menu Fixo");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.ecommerce {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.ecommerce to javafx.fxml;
-    exports com.example.ecommerce;
-}
